62b286e7c50c157b32c4cbf23fa2064d8ec7ab69
Yaseen048 <92057161+Yaseen048@users.noreply.github.com>
2021-11-17 16:13:23 +0000

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 
 def main():
-    
+    prepared_data_url = "https://github.com/ucl-comp0035/coursework-1-Yaseen048/blob/master/Prepared_dataset.csv"
+    df = pd.read_csv(prepared_data_url)
 
 if __name__ == '__main__':
     main()
