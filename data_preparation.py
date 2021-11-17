12429e86ed01770861afbf76fd89125bd6fd1781
Yaseen <yas484@hotmail.com>
2021-11-17 00:05:18 +0000

    update data preparation code

--- data_preparation.py
+++ data_preparation.py
@@ -2,14 +2,6 @@
 
 import pandas as pd
 
-df_March_2020 = pd.read_excel('data\weekend-box-office-report-2020-03-13-15.xls', 
-skiprows = 1)
-
-df_Aug_2020 = pd.read_excel('data\weekend-box-office-report-2020-08-28-30.xls', 
-skiprows = 1)
-
-df_July_2021 = pd.read_excel('data\weekend-box-office-report-2021-07-23-25.xls', 
-skiprows = 1)
 
 def clean_data(data_file):
     data_file.drop(data_file.iloc[:,10:], inplace=True, axis=1)
@@ -17,11 +9,22 @@ def clean_data(data_file):
 
 
 def main():
+    df_March_2020 = pd.read_excel('data\weekend-box-office-report-2020-03-13-15.xls', 
+    skiprows = 1)
+
+    df_Aug_2020 = pd.read_excel('data\weekend-box-office-report-2020-08-28-30.xls', 
+    skiprows = 1)
+
+    df_July_2021 = pd.read_excel('data\weekend-box-office-report-2021-07-23-25.xls', 
+    skiprows = 1)
+    
     clean_data(df_March_2020)
+    clean_data(df_Aug_2020)
+    clean_data(df_July_2021)
+
+
     print(df_March_2020)
 
-#df_March_2020.drop(df_March_2020.iloc[:,10:], inplace=True, axis=1)
-#df_March_2020.drop(df_March_2020.index[16:], inplace=True)
+
 if __name__ == '__main__':
     main()
-#print(df_March_2020)
